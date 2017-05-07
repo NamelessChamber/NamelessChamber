3f79158e2a7fa404ee741de406d3d07b70f05583
Cole Brown <bigswim@gmail.com>
2017-05-06 22:05:34 -0400

    Update migration

--- db/migrate/20170427205652_add_relationships_to_psets.rb
+++ db/migrate/20170427205652_add_relationships_to_psets.rb
@@ -1,10 +1,5 @@
 class AddRelationshipsToPsets < ActiveRecord::Migration[5.0]
   def change
-    change_table :exercises do |t|
-      t.references :p_set
-      t.references :user
-    end
-
     change_table :p_sets do |t|
       t.references :user
     end
