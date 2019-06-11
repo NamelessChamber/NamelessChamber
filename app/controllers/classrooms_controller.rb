f03f3af17880de80d1a75df8d58702f394e3d6ee
Gauna20 <rgauna2016@gmail.com>
2019-06-11 05:51:22 -0700

    Commented out the start/end date in controllers

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -15,14 +15,15 @@ class ClassroomsController < ApplicationController
       .first
 
     @classroom_psets = ClassroomPset
-      .where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+      #.where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
       .where(classroom: @classroom)
       .joins(:p_set)
       .joins('LEFT JOIN p_set_answers ON p_set_answers.p_set_id = p_sets.id')
       .select('p_set_answers.completed AS completed')
       .select('p_sets.id AS p_set_id, p_sets.name AS p_set_name')
       .select('classroom_psets.*')
-      .order('end_date ASC')
+      .order('created_at ASC')
+	  #.order('end_date ASC')
       .to_a
   end
 end
