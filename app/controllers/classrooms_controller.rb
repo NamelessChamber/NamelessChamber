4d045bc221ba1c18a27c1525caa613693a1a336a
Gauna20 <rgauna2016@gmail.com>
2019-06-13 14:01:06 +0200

    fixed line comments that prevented app from running

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -15,15 +15,13 @@ class ClassroomsController < ApplicationController
       .first
 
     @classroom_psets = ClassroomPset
-      #.where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
-      .where(classroom: @classroom)
+      .where(classroom: @classroom) #.where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
       .joins(:p_set)
       .joins('LEFT JOIN p_set_answers ON p_set_answers.p_set_id = p_sets.id')
       .select('p_set_answers.completed AS completed')
       .select('p_sets.id AS p_set_id, p_sets.name AS p_set_name')
       .select('classroom_psets.*')
-      .order('created_at ASC')
-	  #.order('end_date ASC')
+      .order('created_at ASC') #.order('end_date ASC')
       .to_a
   end
 end
