df5be39f0236be3b7a7329c76b7ab6e431e6d0e1
Gauna20 <rgauna2016@gmail.com>
2019-06-19 14:49:00 +0200

    Had to separate each 'before_action', if not then instructor would be given the student view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   # protect_from_forgery unless: -> { request.format.json? }
-  before_action :prefetch_admin_status, :configure_permitted_parameters, if: :devise_controller?
+  before_action :prefetch_admin_status
+  before_action :configure_permitted_parameters, :if => :devise_controller?
 
   protected
   def configure_permitted_parameters
