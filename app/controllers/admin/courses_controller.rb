681a24c122369aaf8566de5052dcaad5190cf5cd
Gauna20 <rgauna2016@gmail.com>
2019-06-19 17:22:18 +0200

    Can now create courses and it will show up in the instructors course/index view

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -10,6 +10,19 @@ class Admin::CoursesController < ApplicationController
   def new
   end
 
+  def create
+	p =	params[:course].permit(:name)
+	course = Course.create(p)
+	# Janky way of creating a course_user
+	# Current Issue:
+	# After creating a new course, the instructor not see it in courses/index
+	course_user = CourseUser.create
+	course_user.user_id = current_user.id
+	course_user.course_id = course.id
+	course_user.save
+	redirect_to admin_course_path(course)
+  end
+
   def update
     begin
       @course = Course.find(params[:id])
