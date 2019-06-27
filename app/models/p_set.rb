d72dfe4fa85954ee92de012f4c1ff7504ff095a1
Gauna20 <rgauna2016@gmail.com>
2019-06-27 18:25:59 +0200

    PSet deletion will delete associations too. Not sure what to do with p_set_answer. Seems like we might want to keep those

--- app/models/p_set.rb
+++ app/models/p_set.rb
@@ -17,8 +17,8 @@ class PSet < ApplicationRecord
   belongs_to :exercise_subcategory
   has_many :classroom_psets, :dependent => :destroy
   has_many :classrooms, :through => :classroom_psets
-  has_many :p_set_answers
-  has_many :p_set_to_audio
+  has_many :p_set_answers # TODO: Should we delete answers?
+  has_many :p_set_to_audio, dependent: :destroy
   has_many :p_set_audios, :through => :p_set_to_audio
 
   serialize :data, JSON
