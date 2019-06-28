a89e721743f2f05540b74bb4d40c7389a3513c5e
Gauna20 <rgauna2016@gmail.com>
2019-06-28 13:31:40 +0200

    Students no longer see other student's pset submissions

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -19,6 +19,7 @@ class ClassroomsController < ApplicationController
       .joins(:p_set)
       .joins('LEFT JOIN p_set_answers ON p_set_answers.p_set_id = p_sets.id')
       .select('p_set_answers.completed AS completed')
+			.select('p_set_answers.user_id AS user_id')
       .select('p_sets.id AS p_set_id, p_sets.name AS p_set_name')
       .select('classroom_psets.*')
       .order('created_at ASC')
