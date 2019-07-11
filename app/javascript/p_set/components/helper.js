e88dc0efdc1763a16e62a1802a065f40d5554d36
Gauna20 <rgauna2016@gmail.com>
2019-07-11 18:03:06 +0200

    Audio player helper now handles multiple audios. Acts on all audio player at the same time. May want to add support for targeting a specific player

--- app/javascript/p_set/components/helper.js
+++ app/javascript/p_set/components/helper.js
@@ -1,11 +1,18 @@
+// Supports multiple audio players
+// Pauses all audio players if at least one is playing
+// Plays all audio players if all are paused
 export function changeAudioPlayerState(){
-    var player = document.getElementsByClassName("react-audio-player")[0];
-    if (player == undefined){
-      return;
-    }
-    if (player.paused){
-      player.play();
-    } else {
-      player.pause();
+    var players = document.getElementsByClassName("react-audio-player");
+    if (!players.length){ return; }
+    var paused = true;
+
+    Array.prototype.forEach.call(players, function(player){
+      paused = paused && player.paused;
+    });
+
+    if (paused){ 
+      Array.prototype.forEach.call(players, function(player) {player.play();});
+    } else{
+      Array.prototype.forEach.call(players, function(player) {player.paused();});
     }
   }
