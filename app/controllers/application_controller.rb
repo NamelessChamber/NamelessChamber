990c62ca087683df2d28e28bc421d4d4892e7e9a
Philippe Dionne <dionne.phil@gmail.com>
2021-01-18 17:45:30 -0500

    Fix error rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def not_found
     respond_to do |format|
-      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+      format.html { render file: Rails.root.join('public/404.html'), layout: false, status: :not_found }
       format.json { head :not_found }
       format.any  { head :not_found }
     end
